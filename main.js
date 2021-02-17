866fc51e8f8770fef44f03026bcf86178194fc3e
merlynnvp <72741790+merlynnvp@users.noreply.github.com>
2021-02-17 22:03:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:100
+});
+
+camera = document.getElementById("camera");
+
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+    });
+}
+
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded); 
+function modelLoaded()
+{
+    console.log("model is loaded");
+}
