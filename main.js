ae30ac2262b88a0d6bf77e8130263cc089eb0dd3
merlynnvp <72741790+merlynnvp@users.noreply.github.com>
2021-02-24 15:22:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,3 +24,19 @@ function modelLoaded()
 {
     console.log("model is loaded");
 }
+
+function check()
+{
+    img = document.getElementById('capture_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+if (error) {
+    console.error(error);
+} else {
+    console.log(result);
+    document.getElementById("result_family_name").innerHTML = results[0].label;
+    document.getElementById("result_family_accuracy").innerHTML = results[0].confidence.toFixed(3);
+ }
+}
